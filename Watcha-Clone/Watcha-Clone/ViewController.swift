b7007e7217029b1e70227ee70e102dfcd9575d91
sumchive <haon0319@naver.com>
2026-04-21 01:33:56 +0900

    [setting] 프로젝트 기초 세팅

---
+++ Watcha-Clone/Watcha-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Watcha-Clone
+//
+//  Created by mandoo on 4/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
