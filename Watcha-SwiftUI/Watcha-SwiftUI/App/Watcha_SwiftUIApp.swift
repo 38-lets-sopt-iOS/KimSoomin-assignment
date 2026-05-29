3a8f285a14c38c2ca12548a9ada9bbb1bc7fc8af
sumchive <haon0319@naver.com>
2026-05-29 17:47:00 +0900

    [setting] SwiftUI 프로젝트 세팅

---
+++ Watcha-SwiftUI/Watcha-SwiftUI/App/Watcha_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watcha_SwiftUIApp.swift
+//  Watcha-SwiftUI
+//
+//  Created by mandoo on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Watcha_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeView()
+        }
+    }
+}
